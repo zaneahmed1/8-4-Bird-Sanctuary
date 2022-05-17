b1333c5bc2320e7e147502bf918e65001cc439a2
Zane <zaneahmed4@gmail.com>
2022-05-16 22:06:31 -0400

    another commit

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,30 +1,81 @@
 import React, { useState } from "react";
 //import BirdCard from "./BirdCard";
+import bonusItems from "../data/bonusItems";
 
 
 
 const Cart = ({cart}) => {
     debugger
-    // const [total, setTotal] = useState(0)
+    
+    const [deleteButton, setDeleteButton] = useState(cart)
 
-    // const handleTotal = (event) => {
-    //     event.preventDefault()
-    //     cart.map(({amount}) => {
-    //         setTotal(total += amount)
-    //     })
-    // }
+    const handleDeleteButton = (index) => {
+        setDeleteButton(deleteButton.splice(index, 1))
+    }
+
+
+    let total = 0
+    cart.forEach(element => {
+       total += element.amount
+       return total
+    });
+
+    const bonuses = () => {
+        if (total > 100 && total <= 300) {
+          return <li>{bonusItems[0]}</li>;
+        } else if (total >= 301 && total <= 500) {
+          return (
+            <>
+              <li>{bonusItems[0]}</li>
+              <li>{bonusItems[1]}</li>
+            </>
+          );
+        } else if (total >= 501 && total <= 1000) {
+          return (
+            <>
+              <li>{bonusItems[0]}</li>
+              <li>{bonusItems[1]}</li>
+              <li>{bonusItems[2]}</li>
+            </>
+          );
+        } else if (total > 1000) {
+          return (
+            <>
+              <li>{bonusItems[0]}</li>
+              <li>{bonusItems[1]}</li>
+              <li>{bonusItems[2]}</li>
+              <li>{bonusItems[3]}</li>
+            </>
+          );
+        }
+      };
+
+    const discounts = () => {
+       if(cart.length >= 3){
+           return (total *= 0.9), (<h4>Discount: 10%</h4>)
+       } else {
+           return (<h4>Discount: 0%</h4>)
+       }
+    } 
+
 
     return (
         <div className="Cart">
             <ol>
             {cart.map(({name, amount})=>{
                 return(
-                <li>{name} - {amount}</li>
+                <li>
+                    {name} - {amount}
+                <button onClick={handleDeleteButton}>Delete</button>
+                </li>
                 )
             })}       
             </ol>
-            <h4>Discount: </h4>
-            <h4>Total : $ </h4>
+            {discounts()}
+            <h4>Total : ${total} </h4>
+            <ul>
+            {bonuses()}
+            </ul>
         </div>
     )
 }
